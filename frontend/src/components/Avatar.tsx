e38fb852975288fab92f6104eae0df25f6c945f5
Utkarsh Awasthi <utkarshawasthi100@gmail.com>
2025-06-23 17:22:09 +0530

    successfully rendered authorName on frontend by updating logic on backend and rectified the commons package

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -8,7 +8,7 @@ interface AvatarProps {
 export const Avatar = ({name , width , height} : AvatarProps) => {
   return (
     <div className = {`w-${width} h-${height} relative inline-flex items-center justify-center  overflow-hidden bg-gray-300 rounded-full`}>
-      <span className = "font-medium text-gray-600 text-md">{(name && name.length > 0) ? name[0] : 'A'}</span>
+      <span className = "font-medium text-gray-600 text-md">{(name && name.length > 0) ? (name[0]).toUpperCase() : "A"}</span>
     </div>
   )
 }
